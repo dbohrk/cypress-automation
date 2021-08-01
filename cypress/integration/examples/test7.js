b257ee6d75ee21e1f50c9849a0b2b6b62a6402d3
Dave Bohrk <dabohrk@twilio.com>
2021-08-01 13:05:40 -0400

    Updates for iframe tests

--- cypress/integration/examples/test7.js
+++ cypress/integration/examples/test7.js
@@ -1,14 +1,28 @@
-/// <reference types="Cypress" />
+/// <reference types="Cypress" /> 
+/// <reference types="cypress-iframe" />
+import 'cypress-iframe'
 
 describe('My First Test Site', function()
 {
     it('My First Test Case',function() {
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
-        cy.get('#opentab').then(function(el){
-        // .prop is not a Cypress method, so .then is used
-            const url=el.prop('href')
-            cy.log(url)
-            cy.visit(url)
-        })
+        cy.frameLoaded('#courses-iframe')
+        // Switch to iframe mode
+        // Find the first occirance of mentorship and click
+        cy.iframe().find("a[href*='mentorship']").eq(0).click()
+        cy.iframe().find("h1[class*='pricing-title']").should('have.length',2)
+    })
 })
-})
+
+// Open a dirrerent URL in the same tab (alternate method)
+// describe('My First Test Site', function () {
+//     it('My First Test Case', function () {
+//         cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+//         cy.get('#opentab').then(function (el) {
+//             // .prop is not a Cypress method, so .then is used
+//             const url = el.prop('href')
+//             cy.log(url)
+//             cy.visit(url)
+//         })
+//     })
+// })
